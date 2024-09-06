b20b09c736e046a74d876a7daddaf6b68498ab0a
unknown <Mountain-Michaelle>
2024-09-06 04:58:21 +0100

    added python spcification for backports

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[BASE_DIR/'static/']
@@ -139,6 +140,7 @@ STATICFILES_DIRS=[BASE_DIR/'static/']
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 MEDIA_URL = 'media/'
