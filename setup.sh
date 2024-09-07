d536bd8b6e10fab78c024fcbaa1f1e0244dd4198
unknown <Mountain-Michaelle>
2024-09-07 03:31:09 +0100

    uncommented staticfiles

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 
-# Build the project
-echo 'Building the project...'
+# Install dependency
 pip install setuptools
 pip install -r requirements.txt
 
-
-
-echo "Make migrations..."
+#Run Django Command
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
